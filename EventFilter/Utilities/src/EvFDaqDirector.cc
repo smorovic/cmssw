fbadc1a20d5f8f94cec5d0d59802ff0356fbba59
Srecko <srecko.morovic@cern.ch>
2023-11-22 21:37:42 +0100

    fix autofs mount (mkdir could fail first time)

--- EventFilter/Utilities/src/EvFDaqDirector.cc
+++ EventFilter/Utilities/src/EvFDaqDirector.cc
@@ -272,6 +272,9 @@ namespace evf {
       auto waitForDir = [=](std::string const& bu_base_dir) -> void {
         int cnt = 0;
         while (!edm::shutdown_flag.load(std::memory_order_relaxed)) {
+	  //stat should trigger autofs mount (mkdir could fail with access denied first time)
+          struct stat statbuf;
+          stat(bu_base_dir.c_str(), &statbuf);
           int retval = mkdir(bu_base_dir.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
           if (retval != 0 && errno != EEXIST) {
             usleep(500000);
@@ -287,9 +290,14 @@ namespace evf {
       };
 
       if (!bu_base_dirs_all_.empty()) {
-        checkExists(bu_base_dirs_all_[0]);
-        bu_run_dir_ = bu_base_dirs_all_[0] + "/" + run_string_;
-        for (unsigned int i = 1; i < bu_base_dirs_all_.size(); i++)
+	if (bu_base_dir_.empty()) {
+          checkExists(bu_base_dirs_all_[0]);
+          bu_run_dir_ = bu_base_dirs_all_[0] + "/" + run_string_;
+	} else {
+          checkExists(bu_base_dir_);
+          bu_run_dir_ = bu_base_dir_ + "/" + run_string_;
+	}
+        for (unsigned int i = 0; i < bu_base_dirs_all_.size(); i++)
           waitForDir(bu_base_dirs_all_[i]);
       } else {
         checkExists(bu_base_dir_);
