e70d70197a40d2670861bdb17a0ad6eab4f6e17f
Srecko <srecko.morovic@cern.ch>
2023-12-08 17:23:49 +0100

    working version (without MVA facilities). sample HLT menu with a HLT path (no preselection at this point)

--- EventFilter/Utilities/plugins/TestProducer.cc
+++ EventFilter/Utilities/plugins/TestProducer.cc
@@ -11,44 +11,61 @@
 #include "DataFormats/Common/interface/AssociationMap.h"
 
 
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 
-TestProducer::TestProducer(edm::ParameterSet const& config) :
-      candToken_(consumes<trigger::TriggerFilterObjectWithRefs>(config.getParameter<edm::InputTag>("candTag"))),
-      tokenR9_(consumes<reco::RecoEcalCandidateIsolationMap>(config.getParameter<edm::InputTag>("inputTagR9"))), //cms.InputTag( 'hltEgammaR9IDUnseeded','r95x5' )
-      tokenHoE_(consumes<reco::RecoEcalCandidateIsolationMap>(config.getParameter<edm::InputTag>("inputTagHoE"))), //cms.InputTag( "hltEgammaHoverE" )
-      tokenSigmaiEtaiEta_(consumes<reco::RecoEcalCandidateIsolationMap>(config.getParameter<edm::InputTag>("inputTagSigmaiEtaiEta"))), //('hltEgammaClusterShapeUnseeded','sigmaIEtaIEta5x5NoiseCleaned' or 'sigmaIEtaIEta5x5' only)
-      tokenIso_(consumes<reco::RecoEcalCandidateIsolationMap>(config.getParameter<edm::InputTag>("inputTagIso"))), //cms.InputTag( "hltEgammaEcalPFClusterIsoUnseeded" ) or cms.InputTag( "hltEgammaHollowTrackIsoUnseeded" ) ?
-
-      ecalRechitEBToken_(consumes(config.getParameter<edm::InputTag>("ecalRechitEB"))),
+MVATestProducer::MVATestProducer(edm::ParameterSet const& config) :
+      //if getting cands from a filter
+      //candToken_(consumes<trigger::TriggerFilterObjectWithRefs>(config.getParameter<edm::InputTag>("candTag"))), //if we consume filter output
+      //else
+      candToken_(consumes<reco::RecoEcalCandidateCollection>(config.getParameter<edm::InputTag>("candTag"))), //if we consume a producer
+      //endif
+      tokenR9_(consumes<reco::RecoEcalCandidateIsolationMap>(config.getParameter<edm::InputTag>("inputTagR9"))),
+//        edm::InputTag("hltEgammaR9IDUnseeded", "r95x5"))),
+      tokenHoE_(consumes<reco::RecoEcalCandidateIsolationMap>(config.getParameter<edm::InputTag>("inputTagHoE"))),
+//        edm::InputTag("hltEgammaHoverE"))),
+      tokenSigmaiEtaiEta_(consumes<reco::RecoEcalCandidateIsolationMap>(config.getParameter<edm::InputTag>("inputTagSigmaiEtaiEta"))),
+//        edm::InputTag("hltEgammaClusterShapeUnseeded", "sigmaIEtaIEta5x5NoiseCleaned"))),
+      tokenIso_(consumes<reco::RecoEcalCandidateIsolationMap>(config.getParameter<edm::InputTag>("inputTagIso"))),
+//       edm::InputTag( "hltEgammaEcalPFClusterIsoUnseeded" )))// or cms.InputTag( "hltEgammaHollowTrackIsoUnseeded" )
+      ecalRechitEBToken_(consumes(config.getParameter<edm::InputTag>("ecalRechitEB"))), //will move to ClusterShapeProducer
       ecalRechitEEToken_(consumes(config.getParameter<edm::InputTag>("ecalRechitEE"))),
       ecalClusterToolsESGetTokens_(consumesCollector())
   {
     produces<std::vector<float>>();
   }
 
-  void TestProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  void MVATestProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
     edm::ParameterSetDescription desc;
     desc.add<edm::InputTag>("candTag");
-    desc.add<edm::InputTag>("inputTagR9");
-    desc.add<edm::InputTag>("inputTagHoE");
-    desc.add<edm::InputTag>("inputTagSigmaiEtaiEta");
+    desc.add<edm::InputTag>("inputTagR9", edm::InputTag("hltEgammaR9IDUnseeded", "r95x5"));
+    desc.add<edm::InputTag>("inputTagHoE", edm::InputTag("hltEgammaHoverEUnseeded"));
+    desc.add<edm::InputTag>("inputTagSigmaiEtaiEta", edm::InputTag("hltEgammaClusterShapeUnseeded", "sigmaIEtaIEta5x5NoiseCleaned"));
+    desc.add<edm::InputTag>("inputTagIso", edm::InputTag("hltEgammaEcalPFClusterIsoUnseeded"));
   }
 
-  void TestProducer::produce(edm::Event& event, edm::EventSetup const& setup) {
+  void MVATestProducer::produce(edm::Event& event, edm::EventSetup const& setup) {
 
-    // Ref to Candidate object to be recorded in filter object
-    edm::Ref<reco::RecoEcalCandidateCollection> ref;
+    //ifdef we get cands from a filter
+//    // Ref to Candidate object to be recorded in filter object
+//    edm::Ref<reco::RecoEcalCandidateCollection> ref;
+//
+//    edm::Handle<trigger::TriggerFilterObjectWithRefs> PrevFilterOutput;
+//    event.getByToken(candToken_, PrevFilterOutput);
+//
+//    std::vector<edm::Ref<reco::RecoEcalCandidateCollection> > recoecalcands;
+//
+//    PrevFilterOutput->getObjects(trigger::TriggerCluster, recoecalcands);
+//    if (recoecalcands.empty())
+//      PrevFilterOutput->getObjects(trigger::TriggerPhoton, recoecalcands);
 
-    //prefiltered collection
-    edm::Handle<trigger::TriggerFilterObjectWithRefs> PrevFilterOutput;
-    event.getByToken(candToken_, PrevFilterOutput);
 
-    //all cands?
-    std::vector<edm::Ref<reco::RecoEcalCandidateCollection> > recoecalcands;
-    //PrevFilterOutput->getObjects(TriggerCluster, recoecalcands);
-    //if (recoecalcands.empty())
-    //probably this collection
-    PrevFilterOutput->getObjects(trigger::TriggerPhoton, recoecalcands);
+    //else
+    edm::Handle<reco::RecoEcalCandidateCollection> recCollection; // "hltEgammaCandidatesUnseeded"
+    event.getByToken(candToken_, recCollection);
+
+    //endif
+
+
 
     //get hold of r9 association map
     edm::Handle<reco::RecoEcalCandidateIsolationMap> r9Map;
@@ -70,21 +87,42 @@ TestProducer::TestProducer(edm::ParameterSet const& config) :
     EcalClusterLazyTools lazyTools(event, ecalClusterToolsESData, ecalRechitEBToken_, ecalRechitEEToken_);
 
     // look at all photons, check cuts and add to filter object
-    for (unsigned int i = 0; i < recoecalcands.size(); i++) {
-      ref = recoecalcands[i];
+    //
+
+
+
+    //if taking trigger cands
+//    for (unsigned int i = 0; i < recoecalcands.size(); i++) {
+//      ref = recoecalcands[i];
+//      //edm::Ref<reco::RecoEcalCandidateCollection> ref(recoecalcands, i);?
+//
+//
+//  #else
+    for (size_t i=0;i < recCollection->size(); i++) {
+      edm::Ref<reco::RecoEcalCandidateCollection> ref(recCollection, i);
+
+    //endif
+
+      edm::LogWarning("MVATestProducer") << " cand 1";
 
       float EtaSC = ref->eta();
       float PhiSC = ref->phi();
 
       //reco::RecoEcalCandidateIsolationMap::const_iterator r9i = (*r9Map).find(ref);
+      edm::LogWarning("MVATestProducer") << " cand 2";
       float r9 = (*r9Map).find(ref)->val;
+      edm::LogWarning("MVATestProducer") << " cand 3";
       float hoe = (*hoEMap).find(ref)->val;
+      edm::LogWarning("MVATestProducer") << " cand 4";
       float siEtaiEta = (*sigmaiEtaiEtaMap).find(ref)->val;
+      edm::LogWarning("MVATestProducer") << " cand 5";
       float iso = (*isoMap).find(ref)->val;
+      edm::LogWarning("MVATestProducer") << " cand 5";
 
       float rawE = ref->superCluster()->rawEnergy();
       float etaW = ref->superCluster()->etaWidth();
       float phiW = ref->superCluster()->phiWidth();
+      edm::LogWarning("MVATestProducer") << " cand 6";
 
       float scEnergy = ref->superCluster()->energy();
       float scEt = scEnergy * sin(2 * atan(exp(-EtaSC)));
@@ -93,16 +131,19 @@ TestProducer::TestProducer(edm::ParameterSet const& config) :
       if (scEt < 0.)
         scEt = 0.; /* first and second order terms assume non-negative energies */
 
+      edm::LogWarning("MVATestProducer") << " cand 7";
       //calculate maximum energy 2x2 cluster in 3x3
       float s4 = lazyTools.s4(*(ref->superCluster()->seed()));
+      edm::LogWarning("MVATestProducer") << " cand 8";
 
       //TODO: calculate S4 -> possibly make a new egamma producer and/or implement S4 in EcalLazyTools which needs access to rechits
 
       //TODO: do MVA calculation with cand pairs
-      std::cout << EtaSC << " " << PhiSC << " " << r9 << " " << hoe << " "
+      edm::LogWarning("DiphotonMVAMVATestProducer") << EtaSC << " " << PhiSC << " " << r9 << " " << hoe << " "
 	        << siEtaiEta << " " << iso << " " << rawE << " " << etaW << " "
-		<< phiW << " " << scEt << " " << s4 << std::endl;
+		<< phiW << " " << scEt << " " << s4;
     }
+      edm::LogWarning("DiphotonMVAMVATestProducer") << "END";
 
     //TODO :nsert here MVA results into a vector pushed here
     std::unique_ptr<std::vector<float>> pi(new std::vector<float>());
@@ -111,4 +152,4 @@ TestProducer::TestProducer(edm::ParameterSet const& config) :
   }
 
 #include "FWCore/Framework/interface/MakerMacros.h"
-DEFINE_FWK_MODULE(TestProducer);
+DEFINE_FWK_MODULE(MVATestProducer);
