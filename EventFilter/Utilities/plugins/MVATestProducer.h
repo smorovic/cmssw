77afbcd0d26eb870bcccfdd51ac0ad5be37229bc
Srecko <srecko.morovic@cern.ch>
2023-12-28 19:27:18 +0100

    updates to test producer (writing a tree)

--- EventFilter/Utilities/plugins/MVATestProducer.h
+++ EventFilter/Utilities/plugins/MVATestProducer.h
@@ -1,7 +1,6 @@
 #ifndef EVENTFILTER_UTILITIES_PLUGINS_TESTPRODUCER
 #define EVENTFILTER_UTILITIES_PLUGINS_TESTPRODUCER
 
-#include <vector>
 
 #include <FWCore/Framework/interface/stream/EDProducer.h>
 #include <FWCore/Framework/interface/Event.h>
@@ -13,6 +12,11 @@
 
 #include "EventFilter/Utilities/interface/photonMvaEstimator.h"
 
+#include <vector>
+
+class TFile;
+class TTree;
+
 namespace edm {
   class ConfigurationDescriptions;
 }
@@ -20,7 +24,7 @@ namespace edm {
 class MVATestProducer : public edm::stream::EDProducer<> {
 public:
   explicit MVATestProducer(edm::ParameterSet const &);
-  ~MVATestProducer() override {}
+  ~MVATestProducer();
 
   static void fillDescriptions(edm::ConfigurationDescriptions &descriptions);
 
@@ -41,6 +45,24 @@ private:
   std::unique_ptr<const photonMvaEstimator> mvaEstimatorB_;
   std::unique_ptr<const photonMvaEstimator> mvaEstimatorE_;
 
+  TFile *f_ = nullptr;
+  TTree *t_ = nullptr;
+
+  uint64_t eventId_ = 0;
+  std::vector<float> *et_;
+  std::vector<float> *scEt_;
+  std::vector<float> *phi_;
+  std::vector<float> *r9_;
+  std::vector<float> *siEtaiEta_;
+  std::vector<float> *rawEnergy_;
+  std::vector<float> *etaW_;
+  std::vector<float> *phiW_;
+  std::vector<float> *e2x2_;
+  std::vector<float> *eta_;
+  std::vector<float> *hoe_;
+  std::vector<float> *iso_;
+  std::vector<float> *mvaScore_;
+ 
 };
 
 #endif
