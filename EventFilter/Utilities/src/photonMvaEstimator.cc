39b0c2a662b89bda18cafda3719ce422245066dd
Srecko <srecko.morovic@cern.ch>
2024-01-09 21:07:54 +0100

    fixed xgboost parameter and more updates

--- EventFilter/Utilities/src/photonMvaEstimator.cc
+++ EventFilter/Utilities/src/photonMvaEstimator.cc
@@ -4,17 +4,15 @@
 #include "CommonTools/MVAUtils/interface/GBRForestTools.h"
 
 #include "xgboost/c_api.h"
+#include "xgboost/learner.h"
+
+constexpr int best_ntree_limit = 48;
 
 photonMvaEstimator::photonMvaEstimator(const edm::FileInPath& weightsfile, const edm::FileInPath& weightsFileXgb){
-  //xgboost::Learner *xgb_model = xgboost::Learner::Create({&xgb_train});
   gbrForest_ = createGBRForest(weightsfile);
-
   XGBoosterCreate(NULL, 0, &booster_);
-  //XGBoosterCreate(NULL, 0, &boosterB_);
-  //XGBoosterCreate(NULL, 0, &boosterE_);
+  std::cout << weightsFileXgb.fullPath().c_str() << std::endl;
   XGBoosterLoadModel(booster_, weightsFileXgb.fullPath().c_str());
-  //XGBoosterLoadModel(boosterB_, "/home/smorovic/CMSSW/CMSSW_13_3_0/src/EventFilter/Utilities/test/barrel.bin");
-  //XGBoosterLoadModel(boosterE_, "/home/smorovic/CMSSW/CMSSW_13_3_0/src/EventFilter/Utilities/test/endcap.bin");
 }
 
 photonMvaEstimator::~photonMvaEstimator() {}
@@ -34,6 +32,7 @@ namespace {
 }  // namespace
 
 double photonMvaEstimator::computeMva(float rawEnergyIn, float r9In, float sigmaIEtaIEtaIn, float etaWidthIn, float phiWidthIn, float s4In, float etaIn, float hOvrEIn, float ecalPFIsoIn) const {
+    return 0;
     float var[9];
 
     var[rawEnergy] = rawEnergyIn;
@@ -71,27 +70,51 @@ double photonMvaEstimator::computeMva2(float rawEnergyIn, float r9In, float sigm
 }
 
 double photonMvaEstimator::computeMva3(float rawEnergyIn, float r9In, float sigmaIEtaIEtaIn, float etaWidthIn, float phiWidthIn, float s4In, float etaIn, float hOvrEIn, float ecalPFIsoIn) const {
-    float var[9];
+  float var[9];
 
-    var[rawEnergy] = rawEnergyIn;
-    var[r9]= r9In;
-    var[sigmaIEtaIEta] = sigmaIEtaIEtaIn;
-    var[etaWidth] = etaWidthIn;
-    var[phiWidth] = phiWidthIn;
-    var[s4] = s4In;
-    var[eta] = etaIn;
-    var[hOvrE] = hOvrEIn;
-    var[ecalPFIso] = ecalPFIsoIn;
+  var[rawEnergy] = rawEnergyIn;
+  var[r9]= r9In;
+  var[sigmaIEtaIEta] = sigmaIEtaIEtaIn;
+  var[etaWidth] = etaWidthIn;
+  var[phiWidth] = phiWidthIn;
+  var[s4] = s4In;
+  var[eta] = etaIn;
+  var[hOvrE] = hOvrEIn;
+  var[ecalPFIso] = ecalPFIsoIn;
 
   DMatrixHandle dmat;
-  XGDMatrixCreateFromMat(var, 1, 9, -1, &dmat);
+  XGDMatrixCreateFromMat(var, 1, 9, -999.9f, &dmat);
   bst_ulong out_len;
   const float* out_result;
-  XGBoosterPredict(booster_, dmat, 0, 0, 0, &out_len, &out_result);
-  //if (fabs(etaIn) < 1.5)
-  //XGBoosterPredict(boosterB_, dmat, 0, 0, 0, &out_len, &out_result);
-  //else
-  //  XGBoosterPredict(boosterE_, dmat, 0, 0, 0, &out_len, &out_result);
-  printf("%f\n", out_result[0]);
+  XGBoosterPredict(booster_, dmat, 0, best_ntree_limit, 0, &out_len, &out_result);
+  XGDMatrixFree(dmat);
+//  std::cout << "XGB score: " << out_result[0] << std::endl;
+//  printf("%f\n", out_result[0]);
+  return out_result[0];
+}
+
+double photonMvaEstimator::computeMva4() const {
+  float var[9];
+  var[0] = 116.004;
+  var[1] = 1.02043;
+  var[2] = 0.023201;
+  var[3] = 0.00653047;
+  var[4] = 0.00588042;
+  var[5] = 110.456;
+  var[6] = -1.76507;
+  var[7] = 4.80358;
+  var[8] = 0.47841;
+
+  DMatrixHandle dmat;
+  XGDMatrixCreateFromMat(var, 1, 9, -999.9f, &dmat);
+
+  bst_ulong out_len;
+  const float* out_result;
+
+  XGBoosterPredict(booster_, dmat, 0, best_ntree_limit, 0, &out_len, &out_result);
+  XGDMatrixFree(dmat);
+
+  std::cout << " ===TEST===   LEN: " <<  out_len << std::endl;
+  printf(" ===TEST=== VAL[0]: %f\n", out_result[0]);
   return out_result[0];
 }
