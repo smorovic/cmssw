8a53583bf373e23ca142be4fd0acf34e7af5b438
Srecko <srecko.morovic@cern.ch>
2024-02-16 16:29:21 +0100

    abs --> std::abs (same result) print event ID
    add crab config files!

---
+++ EventFilter/Utilities/test/crab/crabConfig_hgg_winter24.py
@@ -0,0 +1,29 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = 'mva_hgg_w2024_v4'
+
+config.JobType.pluginName = 'Analysis'
+# Name of the CMSSW configuration file
+config.JobType.psetName = 'hltMC_2024.py'
+
+#config.Data.inputDataset = '/EphemeralHLTPhysics0/Run2023C-v1/RAW'
+config.Data.inputDataset = '/GluGluHToGG_M-125_TuneCP5_13p6TeV_powheg-pythia8/Run3Winter24Digi-133X_mcRun3_2024_realistic_v9-v2/GEN-SIM-RAW'
+config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob = 30
+config.Data.publication = False
+# This string is used to construct the output dataset name
+#config.Data.outputDatasetTag = 'CRAB3_Analysis_test1'
+
+# These values only make sense for processing data
+#    Select input data based on a lumi mask
+#
+## config.Data.lumiMask = '/eos/user/c/cmsdqm/www/CAF/certification/Run3/PromptReco/Run3_2022_2023_Golden.json'
+#    Select input data based on run-ranges
+
+## config.Data.runRange = '367260'
+
+config.JobType.outputFiles = ['photon_mva.root']
+
+# Where the output files will be transmitted to
+config.Site.storageSite = 'T2_CH_CERN'
