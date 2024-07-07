17b2b069c8d1c6ec04170e79c656997da87af54a
Srecko <srecko.morovic@cern.ch>
2024-07-08 00:36:03 +0200

    fix two deadlock scenarios

--- EventFilter/Utilities/src/DAQSource.cc
+++ EventFilter/Utilities/src/DAQSource.cc
@@ -436,6 +436,8 @@ evf::EvFDaqDirector::FileStatus DAQSource::getNextDataBlock() {
   //file is finished
   if (currentFile_->bufferPosition_ == currentFile_->fileSize_) {
     readingFilesCount_--;
+    if (fileListMode_)
+      heldFilesCount_--;
     //release last chunk (it is never released elsewhere)
     freeChunks_.push(currentFile_->chunks_[currentFile_->currentChunk_]);
     if (currentFile_->nEvents_ >= 0 && currentFile_->nEvents_ != int(currentFile_->nProcessed_)) {
@@ -1084,10 +1086,6 @@ void DAQSource::readSupervisor() {
 
         //wake up the worker thread
         cvReader_[newTid]->notify_one();
-        lk.unlock();
-
-        //acquiring the lock to wait for thread to receive CV
-        std::unique_lock<std::mutex> lkn(*mReaderNotify_[newTid]);
       }
     }
   }
@@ -1130,12 +1128,8 @@ void DAQSource::readWorker(unsigned int tid) {
     }
     cvWakeup_.notify_all();
 
-    std::unique_lock<std::mutex> lkn(*mReaderNotify_[tid]);
-
     cvReader_[tid]->wait(lk);
     lk.unlock();
-
-    lkn.unlock();
 
     if (thread_quit_signal[tid])
       return;
